0cb33046e3cf79f25fd7192d7b883d7e1e55daa3
OtavioPortela <otavionegocios97@gmail.com>
2023-04-13 14:20:02 -0300

    Trabalhando com bancos de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return  render(request, 'index.html')
 
+
+def index(request):
+    dados = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "Foto do telescopio NASA"},
+        2: {"nome": "Galaxias",
+            "legenda": "Foto do telescopio NASA/ hubble"}
+    }
+
+    return render(request, 'galeria/index.html', {"cards": dados})
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
 
